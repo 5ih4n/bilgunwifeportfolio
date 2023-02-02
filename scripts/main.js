d3b6552883af80bc1d4dd2c03a0c10ff1dfadf08
Sihan Li <5ih4n.san@gmail.com>
2023-02-02 01:19:40 +0100

    Sync

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
-            entry.target.classList.remove('show')
+            entry.target.classList.remove('show');
         }
     });
 });
@@ -11,14 +11,18 @@ const hiddenElements = document.querySelectorAll('.hidden');
 
 hiddenElements.forEach((el) => observer.observe(el));
 
-const images = document.querySelector(".gallery img")
-const modal = document.querySelector(".modal")
-const modalImg = document.querySelector(".modalImg")
-const modalTxt = document.querySelector(".modalTxt")
-const close = document.querySelector(".modalClose")
+const images = document.querySelectorAll(".gallery img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const modalTxt = document.querySelector(".modalTxt");
+const close = document.querySelector(".modalClose");
 
 images.forEach((image) => {
     image.addEventListener("click", () => {
-        modal.classList.add("appear")
-    })
-})
+        modal.classList.add("appear");
+    });
+
+    close.addEventListener("click", () => {
+        modal.classList.remove("appear");
+    });
+});
