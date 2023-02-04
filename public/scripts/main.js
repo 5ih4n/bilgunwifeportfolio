2bc1ed1c8e30809e1a1c7e35e0a046b302fc40c8
Sihan Li <5ih4n.san@gmail.com>
2023-02-04 21:48:13 +0100

    firebase

---
+++ public/scripts/main.js
@@ -0,0 +1,30 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+const images = document.querySelectorAll(".gallery img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const modalTxt = document.querySelector(".modalTxt");
+const close = document.querySelector(".modalClose");
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modal.classList.add("appear");
+        modalImg.src = image.src;
+        modalTxt.innerHTML = image.alt;
+    });
+
+    close.addEventListener("click", () => {
+        modal.classList.remove("appear");
+    });
+});
