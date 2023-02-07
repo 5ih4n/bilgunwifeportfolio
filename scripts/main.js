9a75e19be3d30fd27d82bd49bdd773b2e6e10998
SIhan_Li <5ih4n.san@gmail.com>
2023-02-07 19:59:48 +0100

    strat on mobile optimization

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
+const menuList = document.getElementById("menuList");
+const icon = document.querySelector('.navburger');
+const images = document.querySelectorAll(".gallery img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const modalTxt = document.querySelector(".modalTxt");
+const close = document.querySelector(".modalClose");
+const hiddenElements = document.querySelectorAll('.hidden');
+
+menuList.style.maxHeight = "0px";
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modal.classList.add("appear");
+        modalImg.src = image.src;
+        modalTxt.innerHTML = image.alt;
+        console.log("pung");
+    });
+
+    close.addEventListener("click", () => {
+        modal.classList.remove("appear");
+    });
+});
+
+icon.onclick = () => {
+    icon.classList.toggle('opened');
+    icon.setAttribute('aria-expanded', icon.classList.contains('opened'));
+    menuList.style.maxHeight = menuList.style.maxHeight === "0px" ? "340px" : "0px";
+};
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -7,24 +37,6 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 });
-const hiddenElements = document.querySelectorAll('.hidden');
 
 hiddenElements.forEach((el) => observer.observe(el));
 
-const images = document.querySelectorAll(".gallery img");
-const modal = document.querySelector(".modal");
-const modalImg = document.querySelector(".modalImg");
-const modalTxt = document.querySelector(".modalTxt");
-const close = document.querySelector(".modalClose");
-
-images.forEach((image) => {
-    image.addEventListener("click", () => {
-        modal.classList.add("appear");
-        modalImg.src = image.src;
-        modalTxt.innerHTML = image.alt;
-    });
-
-    close.addEventListener("click", () => {
-        modal.classList.remove("appear");
-    });
-});
