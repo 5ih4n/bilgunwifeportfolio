8f86991ec5a2b0a92073a2ce432427d45019991b
Sihan Li <5ih4n.san@gmail.com>
2023-02-28 00:54:55 +0100

    Test

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,12 +1,20 @@
 const cartHide = document.getElementById("cartHide");
 const cartSection = document.getElementById("cartSection");
 const cartBtn = document.getElementById("cartBtn");
+var addItemButtons = document.getElementsByClassName("buyBtn");
 
+for (var i = 0; i < addItemButtons.length; i++) {
+    var button = addItemButtons[i];
+    button.addEventListener("click", function(event) {
+        var buttonClicked = event.target;
+        buttonClicked.parentElement.remove();
+    });
+};
 
 cartHide.onclick = () => {
     cartSection.style.display = "none";
-}
+};
 
 cartBtn.onclick = () => {
     cartSection.style.display = "block";
-}
+};
