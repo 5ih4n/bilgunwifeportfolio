064e732745b1b3527bda4672022ef830a1d96025
SIhan_Li <5ih4n.san@gmail.com>
2023-02-28 19:48:04 +0100

    shop unfinished addons, sync

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,14 +1,43 @@
 const cartHide = document.getElementById("cartHide");
 const cartSection = document.getElementById("cartSection");
 const cartBtn = document.getElementById("cartBtn");
-var addItemButtons = document.getElementsByClassName("buyBtn");
+var removeButtons = document.getElementsByClassName("removeBtn");
 
-for (var i = 0; i < addItemButtons.length; i++) {
-    var button = addItemButtons[i];
-    button.addEventListener("click", function(event) {
-        var buttonClicked = event.target;
-        buttonClicked.parentElement.remove();
-    });
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+} else {
+    ready();
+};
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName("removeBtn");
+    for (var i = 1; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i];
+        button.addEventListener("click", removeCartItem);
+    };
+
+    var quantityInputs = document.getElementsByClassName("cartQuant");
+    for (var i = 1; i < quantityInputs.length; i++) {
+        var button = removeCartItemButtons[i];
+        button.addEventListener("change", quantityChanged);
+    };
+
+    var addToCartBtns = document.getElementsByClassName("buyBtn");
+    for (var i = 1; i < quantityInputs.length; i++) {
+        var button = addToCartBtns[i];
+        button.addEventListener("click", addToCart);
+    };
+};
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updateCart()
+}
+
+function addToCart(name, price, imgsrc) {
+    var cartRow = document.createElement("div");
+    cartRow.classList.add('cart-row');
 };
 
 cartHide.onclick = () => {
